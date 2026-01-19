031d113d2ebbb8cb91c85ece1a1ab6922c3f424d
Andrew <naizbatkovich@gmail.com>
2026-01-19 14:43:52 +0200

    Added js basics tasks

---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+//4.1
+const circleRadius = 5;
+const circleArea = Math.PI * circleRadius ** 2;
+console.log(circleArea.toFixed(2));
+
+//4.2
+const length = 10;
+const width = 4;
+const rectangleArea = length * width;
+console.log(rectangleArea.toFixed(2));
+
+//4.3
+const cylinderRadius = 3;
+const height = 7;
+const cylinderVolume = Math.PI * cylinderRadius ** 2 * height;
+console.log(cylinderVolume.toFixed(2));
