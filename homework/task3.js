dbaab460ebecd2837c7786625d26361402efab48
Andrew <naizbatkovich@gmail.com>
2026-01-22 13:55:47 +0200

    Added homework for lesson 5

---
+++ homework/task3.js
@@ -0,0 +1,11 @@
+const number = 7;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+
+let i = 1;
+while (i <= 10) {
+    console.log(`${number} x ${i} = ${number * i}`);
+    i++;
+}
