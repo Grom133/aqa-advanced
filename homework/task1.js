72c07b37593d4f7c60880bc61788f81d95b7caab
Andrew <naizbatkovich@gmail.com>
2026-02-19 20:10:39 +0200

    with no errors

--- homework/task1.js
+++ homework/task1.js
@@ -1,13 +1,13 @@
 const averageGrade = 77;
 
 if (averageGrade < 60) {
-    console.log("Незадовільно");
+    console.log("Bad");
 } else if (averageGrade >= 60 && averageGrade <= 70) {
-    console.log("Задовільно");
+    console.log("Not bad");
 } else if (averageGrade >= 71 && averageGrade <= 80) {
-    console.log("Добре");
+    console.log("Good");
 } else if (averageGrade >= 81 && averageGrade <= 90) {
-    console.log("Дуже добре");
+    console.log("Very Good");
 } else if (averageGrade >= 91 && averageGrade <= 100) {
-    console.log("Відмінно");
+    console.log("Excelent");
 }
